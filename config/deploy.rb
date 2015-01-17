2f509c81dc4ef85ec56f36549eacc0431db92d89
deepj <deepjungle.maca@gmail.com>
2015-01-17 16:50:51 +0100

    Improve code style

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,27 @@
 
-set :user, "root"
+set :user, 'root'
 set :use_sudo, false
-set :ssh_options, {forward_agent: true}
-
+set :ssh_options, forward_agent: true
 
 task :clean do
-  #clean stopped containers
-  run "docker rm $(docker ps -a -q)"
-  #clean untagged images
+  # clean stopped containers
+  run 'docker rm $(docker ps -a -q)'
+  # clean untagged images
   run "docker rmi $(docker images| grep \"^<none>\" | awk '{print $3}')"
 end
 
-
 task :install do
-  run "mkdir -p aukro_bot"
-  upload "auctions.yml", "aukro_bot/auctions.yml", :via => :scp
-  upload "aukro.yml", "aukro_bot/aukro.yml", :via => :scp
-  upload "Dockerfile", "aukro_bot/Dockerfile", :via => :scp
+  run 'mkdir -p aukro_bot'
+
+  upload 'auctions.yml', 'aukro_bot/auctions.yml', via: :scp
+  upload 'aukro.yml', 'aukro_bot/aukro.yml',       via: :scp
+  upload 'Dockerfile', 'aukro_bot/Dockerfile',     via: :scp
+
   run 'cd aukro_bot && docker build --rm --no-cache -t="aukro_bot" .'
 end
 
 task :start do
-  run "docker run -d aukro_bot"
+  run 'docker run -d aukro_bot'
 end
 
 task :stop do
